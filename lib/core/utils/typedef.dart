3b15809c1f5a07c8001720deb1c63ddc84ca9acd
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2024-08-20 10:30:40 -0300

    creating failure and typedef

---
+++ lib/core/utils/typedef.dart
@@ -0,0 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_tutorial/core/core.dart';
+
+typedef ResultFuture<T> = Future<Either<Failure, T>>;
+
+typedef ResultVoid = ResultFuture<void>;
--- lib/src/authentication/domain/repositories/authentication_repository.dart
+++ lib/src/authentication/domain/repositories/authentication_repository.dart
@@ -1,14 +1,16 @@
-import 'package:dartz/dartz.dart';
-
+import '../../../../core/core.dart';
 import '../domain.dart';
 
 abstract class AuthenticationRepository {
   const AuthenticationRepository();
 
-  Future<Either<Exception, void>> createUser(
+  // NOTES: Not state the actual failure that you're expecting, because in this way,
+  // when we do this, if the type of failure changes, we don't have to go into
+  // all of our repositories to change them
+  ResultVoid createUser(
       {required String createdAt,
       required String name,
       required String avatar});
 
-  Future<(Exception, List<User>)> getUsers();
+  ResultFuture<List<User>> getUsers();
 }
