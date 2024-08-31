f7eed3e456f79238a5318e6036656d951383b151
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2024-08-31 15:59:51 -0300

    creating authentication bloc

---
+++ lib/src/authentication/presentation/bloc/authentication_state.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/domain.dart';
+
+abstract class AuthenticationState extends Equatable {
+  const AuthenticationState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUser extends AuthenticationState {
+  const GettingUser();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this.users);
+
+  final List<User> users;
+
+  @override
+  List<Object?> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
